042af76185648368fc991ca712e4aa1037f8e7e3
$/humilde <101001858+yulocode@users.noreply.github.com>
2022-06-19 23:07:41 -0300

    main

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    <style>
+        h1 {
+           color: #ff00ff;
+           text-shadow: 1px 1px 1px black;
+        }
+    </style>
+</head>
+<body>
+<?php echo "<h1>olá</br> mundo<h1>"; ?>
+
+
+</body>
+</html>
